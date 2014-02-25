0fbfe9bf17a4f643e447e84b13c8999eaed315e5
t.shakuov <t.shakuov@gmail.com>
2014-02-25 21:48:57 +0400

    Project started!

---
+++ simple/src/main/java/com/mockertim/applet/simple/Simple.java
@@ -0,0 +1,14 @@
+package com.mockertim.applet.simple;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: timur
+ * Date: 25.02.14
+ * Time: 21:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Simple extends JApplet {
+
+}
